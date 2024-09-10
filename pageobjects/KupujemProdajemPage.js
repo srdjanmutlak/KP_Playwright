f51e5cc61fbe6bc44f0627a48a534acd04157af0
srdjanmutlak <84793610+srdjanmutlak@users.noreply.github.com>
2024-09-10 16:53:13 +0200

    Update KupujemProdajemPage.js

--- pageobjects/KupujemProdajemPage.js
+++ pageobjects/KupujemProdajemPage.js
@@ -99,7 +99,7 @@ async chooseConditions(conditions) {
 }
 
 async enterTextInCategoryDropdownWithDelay(text) {
-    await this.categoryDropdownLocator.type(text); 
+    await this.categoryDropdownLocator.type(text, { delay: 100 }); 
     await this.page.keyboard.press('Enter');  
 }
 
