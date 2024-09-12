6dd46813ba901c78afd430166d129e613dd09bf8
srdjanmutlak <84793610+srdjanmutlak@users.noreply.github.com>
2024-09-12 09:07:06 +0200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -3,30 +3,27 @@ const { devices } = require('@playwright/test');
 
 const config = {
   testDir: './tests',
-  retries :3,
+  retries: 3,
   
   /* Maximum time one test can run for. */
   timeout: 100 * 1000,
   expect: {
-  
     timeout: 5000
   },
   
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* HTML report configuration */
+  reporter: [['html', { outputFolder: 'playwright-report' }]], // Definiše gde će se čuvati HTML izveštaj
+
+  /* Shared settings for all the projects below. */
   workers: 4,
   use: {
-
-    browserName : 'chromium',
-    headless : true,
-    screenshot : 'on',
-    trace : 'on',
-    video: 'on-first-retry',
-    
-    
+    browserName: 'chromium',
+    headless: true,
+    screenshot: 'on',  // Snimi screenshot nakon svakog testa
+    trace: 'on',       // Snimi trace za svaku sesiju
+    video: 'on-first-retry',  // Snimi video samo tokom prvog ponovnog pokušaja
+    outputDir: 'test-results/',  // Gde će se čuvati svi izlazni fajlovi (video, screenshot, trace)
   },
-
-
 };
 
 module.exports = config;
