bdd16050505848da223ed41e78dadd80cc94ef90
srdjanmutlak <84793610+srdjanmutlak@users.noreply.github.com>
2024-09-14 10:53:27 +0200

    Update POManager.js

--- pageobjects/POManager.js
+++ pageobjects/POManager.js
@@ -1,60 +1,36 @@
-const {LoginPage} = require('./LoginPage');
-const {DashboardPage} = require('./DashboardPage');
-const {OrdersHistoryPage} = require('./OrdersHistoryPage');
-const {OrdersReviewPage} = require('./OrdersReviewPage');
-const {CartPage} = require('./CartPage');
-const {KupujemProdajemPage} = require('./KupujemProdajemPage');
-const {TestPage} = require('./TestPage');
+const { LoginPage } = require('./loginPage');
+const { HomePage } = require('./homePage');
+const { DetailedSearchPage } = require('./detailedSearchPage');
+const { AdPage } = require('./adPage');
 
-class POManager
-{
-constructor(page)
-{
-    this.page = page;
-    this.loginPage = new LoginPage(this.page);
-    this.dashboardPage = new DashboardPage(this.page);
-    this.ordersHistoryPage = new OrdersHistoryPage(this.page);
-    this.ordersReviewPage = new OrdersReviewPage(this.page);
-    this.cartPage = new CartPage(this.page);
-    this.kupujemProdajemPage = new KupujemProdajemPage(this.page);
+class POManager {
+    constructor(page) {
+        this.page = page;
+        this.loginPage = new LoginPage(this.page);
+        this.homePage = new HomePage(this.page);
+        this.detailedSearchPage = new DetailedSearchPage(this.page);
+        this.adPage = new AdPage(this.page);
+    }
 
-    this.testPage = new TestPage(this.page);
+    getLoginPage() {
+        return this.loginPage;
+    }
 
+    getKupujemProdajemPage() {
+        return this.kupujemProdajemPage;
+    }
+
+    getHomePage() {
+        return this.homePage;
+    }
+
+    getDetailedSearchPage() {
+        return this.detailedSearchPage;
+    }
+
+    getAdPage() {
+        return this.adPage;
+    }
 }
 
-getTestPage()
-{
-    return this.testPage;
-}
-
-
-getLoginPage()
-{
-    return this.loginPage;
-}
-
-getCartPage()
-{
-    return this.cartPage;
-}
-
-getDashboardPage()
-{
-    return this.dashboardPage;
-}
-getOrdersHistoryPage()
-{
-    return this.ordersHistoryPage;
-}
-
-getOrdersReviewPage()
-{
-    return this.ordersReviewPage;
-}
-
-getKupujemProdajem()
-{
-    return this.kupujemProdajemPage;
-}
-}
-module.exports = {POManager};
+module.exports = { POManager };
