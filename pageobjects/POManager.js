088e4a3a77f761123af4afec77c1f880f6a430b6
srdjanmutlak <84793610+srdjanmutlak@users.noreply.github.com>
2024-09-25 20:29:39 +0200

    Update POManager.js

--- pageobjects/POManager.js
+++ pageobjects/POManager.js
@@ -1,3 +1,4 @@
+const { BasePage } = require('./basePage');
 const { LoginPage } = require('./loginPage');
 const { HomePage } = require('./homePage');
 const { DetailedSearchPage } = require('./detailedSearchPage');
@@ -6,10 +7,15 @@ const { AdPage } = require('./adPage');
 class POManager {
     constructor(page) {
         this.page = page;
+        this.basePage = new BasePage(this.page);
         this.loginPage = new LoginPage(this.page);
         this.homePage = new HomePage(this.page);
         this.detailedSearchPage = new DetailedSearchPage(this.page);
         this.adPage = new AdPage(this.page);
+    }
+
+    getBasePage() {
+        return this.basePage;
     }
 
     getLoginPage() {
