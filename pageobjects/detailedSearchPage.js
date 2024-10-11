c2ae63ea78a100fdbcf6db6ef86ef88ee48624a7
srdjanmutlak <84793610+srdjanmutlak@users.noreply.github.com>
2024-10-11 15:44:17 +0200

    Update detailedSearchPage.js

--- pageobjects/detailedSearchPage.js
+++ pageobjects/detailedSearchPage.js
@@ -8,17 +8,31 @@ class DetailedSearchPage extends BasePage {
             categoryDropdown: page.locator("#react-select-categoryId-input"),
             groupDropdown: page.locator("#react-select-groupId-input"),
             priceFromInput: page.locator("#priceFrom"),
+          //  dinRadioButton: page.locator("//label[contains(.,'din')]"),
             onlyWithPriceCheckbox: page.locator("//span[contains(text(), 'Samo sa cenom')]"),
-            conditionInput: page.locator("#react-select-condition-input"),
+            conditionInput: page.locator("#react-select-condition-placeholder"),
             applyFiltersButton: page.locator("//button[@aria-label='Primeni filtere']"),
             searchResultCount: page.locator("//a[contains(text(),'Bluze')]/following-sibling::span"),
         };
     }
 
-    async selectCategoryAndGroup(categoryText, groupText, delay = 0) {
-        await this.selectOption(this.locators.categoryDropdown, categoryText, {}, delay);
-        await this.selectOption(this.locators.groupDropdown, groupText, {}, delay);
+    async clickOnCategory() {
+        await this.click(this.locators.categoryDropdown);
     }
+
+    // async selectCategoryAndGroup(categoryText, groupText, delay = 0) {
+    //     await this.selectOption(this.locators.categoryDropdown, categoryText, {}, delay);
+    //     await this.selectOption(this.locators.groupDropdown, groupText, {}, delay);
+    // }
+
+    async selectCategoryAndGroup(categoryText, groupText) {
+        // Prvo biramo kategoriju
+        await this.selectDropdownOption(categoryText);
+    
+        // Zatim biramo grupu
+        await this.selectDropdownOption(groupText);
+    }
+    
     
     async setPriceFrom(price) {
         await this.fillInput(this.locators.priceFromInput, price);
@@ -32,12 +46,30 @@ class DetailedSearchPage extends BasePage {
         await this.click(this.locators.onlyWithPriceCheckbox);
     }
 
-    async selectConditions(conditionsArray) {
-        for (const condition of conditionsArray) {
-            await this.selectOption(this.locators.conditionInput, condition);
-        }
-    }
+    // async selectConditions(conditionsArray) {
+    //     for (const condition of conditionsArray) {
+    //         await this.selectOption(this.locators.conditionInput, condition);
+    //     }
+    // }
 
+    // type in console to freeze dropdown: setTimeout(() => { debugger; }, 3000);
+    async selectDropdownOption(optionText) {    
+        // Pronalazimo opciju na osnovu teksta i kliknemo na nju
+        const optionLocator = `div[role="option"][aria-selected="false"] div:has-text("${optionText}")`;
+        await this.page.click(optionLocator);
+    }   
+
+    async selectConditions(conditionsArray) {
+        // Klik na glavni input za prikazivanje opcija (ukoliko je to potrebno da bi se prikazale opcije)
+        await this.click(this.locators.conditionInput);
+    
+        // Petlja kroz sve uslove u conditionsArray i klik na odgovarajući checkbox element
+        for (const condition of conditionsArray) {
+            const checkboxLocator = `label:has-text("${condition}")`;
+            await this.page.click(checkboxLocator);
+        }
+    } 
+    
     async applyFilters() {
         await this.click(this.locators.applyFiltersButton);
     }
